4a5f54353d10c1bd07569bc4c6051a992dc94b9c
retr0717 <alwinaji717@gmail.com>
2024-07-31 13:36:25 +0530

    patterns completed

--- patterns.cpp
+++ patterns.cpp
@@ -104,8 +104,8 @@ void print13(int n) {
 void print14(int n) {
   cout << "\n";
 
-  for (int i = 65; i < n; i++) {
-    for (int j = 65; j <= i; j++) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 'A'; j <= 'A' + i; j++) {
       cout << (char)j << " ";
     }
     cout << "\n";
@@ -116,11 +116,10 @@ void print15(int n) {
 
   cout << "\n";
   int k = n;
-  for (int i = 65; i < n; i++) {
-    for (int j = 65; j < k; j++) {
-      cout << (char)j << " ";
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - i; j++) {
+      cout << (char)('A' + j) << " ";
     }
-    k--;
     cout << "\n";
   }
 }
@@ -128,11 +127,148 @@ void print15(int n) {
 void print16(int n) {
   cout << "\n";
 
-  for (int i = 65; i < n; i++) {
-    for (int j = 65; j <= i; j++) {
-      cout << (char)i << " ";
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j <= i; j++) {
+      cout << (char)('A' + i) << " ";
     }
     cout << "\n";
+  }
+}
+
+void print17(int n) {
+  cout << "\n";
+  for (int i = 0; i < n; i++) {
+    // upper trinagle.
+    for (int k = 0; k < n - i - 1; k++) {
+      cout << " ";
+    }
+    // start print.
+    char ch = 'A';
+    int bPoint = ((2 * i) + 1) / 2;
+    for (int j = 0; j < 2 * i + 1; j++) {
+      cout << ch;
+      if (j < bPoint)
+        ch++;
+      else
+        ch--;
+    }
+    cout << "\n";
+  }
+}
+
+void print18(int n) {
+  cout << "\n";
+
+  for (int i = 0; i < n; i++) {
+    for (int j = i; j >= 0; j--)
+      cout << (char)('E' - j) << " ";
+    cout << "\n";
+  }
+}
+
+void print19(int n) {
+  int k = 0;
+
+  // upper symmetry.
+  for (int i = 0; i < n; i++) {
+    cout << "\n";
+    for (int j = 1; j <= n - i; j++) {
+      cout << "*";
+    }
+
+    // print space.
+    for (int j = 0; j < k; j++) {
+      cout << " ";
+    }
+    k = k + 2;
+
+    // print star.
+    for (int j = 1; j <= n - i; j++) {
+      cout << "*";
+    }
+  }
+
+  // lower symmetry.
+  for (int i = 0; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << "*";
+    }
+
+    // print space.
+    for (int j = 0; j < k; j++) {
+      cout << " ";
+    }
+    k = k - 2;
+
+    // print star.
+    for (int j = 1; j <= i; j++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+}
+
+void print20(int n) {
+  int k = (2 * n) - 2;
+  int stars = 0;
+  for (int i = 1; i <= (2 * n) - 1; i++) {
+
+    // cout << "\nk = " << k << " i = " << i;
+    cout << "\n";
+
+    if (i > n)
+      stars = (2 * n) - i;
+    else
+      stars = i;
+
+    for (int j = 1; j <= stars; j++) {
+      cout << "*";
+    }
+
+    // print space.
+    for (int j = 0; j < k; j++) {
+      cout << " ";
+    }
+
+    // print star.
+    for (int j = 1; j <= stars; j++) {
+      cout << "*";
+    }
+
+    if (i < n)
+      k -= 2;
+    else
+      k += 2;
+  }
+
+  cout << "\n";
+}
+
+void print21(int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+        cout << "*";
+      } else {
+        cout << " ";
+      }
+    }
+    cout << endl;
+  }
+}
+
+void print22(int n) {
+  cout << "\n";
+  for (int i = 0; i < 2 * n - 1; i++) {
+    for (int j = 0; j < 2 * n - 1; j++) {
+      int top = i;
+      int left = j;
+      int right = 2 * n - 2 - j;
+      int down = 2 * n - 3 - i;
+
+      cout << n - min(min(top, down), min(left, right));
+    }
+    cout << endl;
   }
 }
 
@@ -143,8 +279,14 @@ int main() {
   print11(9);
   print12(5);
   print13(5);
-  print14(77);
-  print15(77);
-  print16(77);
+  print14(5);
+  print15(5);
+  print16(5);
+  print17(5);
+  print18(5);
+  print19(5);
+  print20(5);
+  print21(5);
+  print22(5);
   return 0;
 }
