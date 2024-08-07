ca7949e76701808b75cf8351cf464bf51ae97638
retr0717 <alwinaji717@gmail.com>
2024-08-07 22:06:37 +0530

    leetcode : binary search

---
+++ binary_search.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int search(vector<int> &nums, int target) {
+
+    int start = 0, end = nums.size() - 1, mid;
+    while (start <= end) {
+      mid = (start + end) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] < target) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+
+    return -1;
+  }
+};
+
+int main() {
+  Solution s;
+  vector<int> nums = {-1, 0, 3, 5, 9, 12};
+  int target = 9;
+  cout << s.search(nums, target) << endl;
+  return 0;
+}
