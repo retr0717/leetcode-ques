3e72954a822459bf024e8363ae7c5d67969d7ac0
retr0717 <alwinaji717@gmail.com>
2024-08-24 15:46:43 +0530

    row with maximum 1 : matrix optimal

--- rowWithMatrix.cpp
+++ rowWithMatrix.cpp
@@ -2,19 +2,35 @@
 #include <vector>
 using namespace std;
 
+int LowerBound(vector<int> &nums, int target) {
+  int start = 0, end = nums.size() - 1;
+  int ans =
+      nums.size(); // Initialize to the size of the array (end of the range)
+  while (start <= end) {
+    int mid = (start + end) / 2;
+    if (nums[mid] >= target) {
+      ans = mid;
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return ans;
+}
+
 int rowWithMax1s(vector<vector<int>> &matrix, int n, int m) {
   int count = 0, maxi = -1, index = 0;
 
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      if (matrix[i][j] == 1)
-        count++;
-    }
+
+    count = m - LowerBound(matrix[i], 1);
 
     if (maxi < count && count != 0) {
       index = i;
       maxi = count;
     }
+
     count = 0;
   }
 
