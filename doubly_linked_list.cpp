a38657d613663806c926b996b6fe42a31972bbb7
retr0717 <alwinaji717@gmail.com>
2024-09-02 18:49:26 +0530

    doublly linked list intro adding to the end

--- doubly_linked_list.cpp
+++ doubly_linked_list.cpp
@@ -1,43 +1,43 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Node {
 public:
   int data;
-  Node *next;
-  Node *prev;
-
-  Node(int data) {
-    this->data = data;
-    this->next = nullptr;
-    this->prev = nullptr;
-  }
+  Node *next, *prev;
+  Node() : data(0), next(nullptr), prev(nullptr) {}
+  Node(int x) : data(x), next(nullptr), prev(nullptr) {}
+  Node(int x, Node *next, Node *prev) : data(x), next(next), prev(prev) {}
 };
 
 void addAtEnd(Node *head, int data) {
+  Node *temp = head;
+  while (temp->next != nullptr)
+    temp = temp->next;
+
   Node *newNode = new Node(data);
-  Node *curr = head;
-  while (curr->next != nullptr) {
-    curr = curr->next;
+  temp->next = newNode;
+  newNode->prev = temp;
+}
+
+Node *constructDLL(vector<int> &arr) {
+
+  Node *head = new Node(arr[0]);
+  for (int i = 1; i < arr.size(); i++) {
+    addAtEnd(head, arr[i]);
   }
-  curr->next = newNode;
-  newNode->prev = curr;
+
+  return head;
 }
 
 int main() {
-  Node *head = new Node(1);
-
-  addAtEnd(head, 2);
-  addAtEnd(head, 3);
-  addAtEnd(head, 4);
-
-  Node *temp = head;
-  while (temp != nullptr) {
-    cout << "addr : " << temp << " | prev : " << temp->prev
-         << " | data : " << temp->data << " | next : " << temp->next << endl;
-    ;
-    temp = temp->next;
+  vector<int> v = {1, 2, 3, 4, 5};
+  Node *head = constructDLL(v);
+  for (Node *curr = head; curr != nullptr; curr = curr->next) {
+    cout << " Node :: " << curr << " || Data :: " << curr->data
+         << " | Next :: " << curr->next << " || Prev :: " << curr->prev
+         << " ||\n";
   }
-
   return 0;
 }
