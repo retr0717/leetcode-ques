abfb2758853ef4dd2052bf8125c51e29c299aea9
retr0717 <alwinaji717@gmail.com>
2024-10-16 22:06:49 +0530

    permutations of a given array without o(n) space

--- recursion/permutations.cpp
+++ recursion/permutations.cpp
@@ -32,13 +32,36 @@ public:
         permutation(nums, ans, ds, freq);
         return ans;
     }
+private:
+    void permutation2(int index, vector<vector<int>>& ans, vector<int>& nums)
+    {
+        if(index == nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i = index ; i < nums.size() ; i++)
+        {
+            swap(nums[index],nums[i]);
+            permutation2(index+1, ans, nums);
+            swap(nums[index], nums[i]);
+        }
+    }
+public:
+    vector<vector<int>> permute2(vector<int>& nums) {
+        vector<vector<int>> ans;
+        permutation2(0, ans, nums);
+        return ans;
+    }
 };
 
 int main()
 {
     vector<int> arr = {1,2,4};
     Solution s;
-    vector<vector<int>> ans = s.permute(arr);
+    //vector<vector<int>> ans = s.permute(arr);
+    vector<vector<int>> ans = s.permute2(arr);
     for(auto s : ans)
     {
         cout<<"[ ";
