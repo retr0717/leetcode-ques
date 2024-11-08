0903f5d3a52c5445de931426e4c527084af35ab4
retr0717 <alwinaji717@gmail.com>
2024-11-08 08:32:58 +0530

    divide two nos without division and mul operators

---
+++ bit/divide_two_nos.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <climits>
+using namespace std;
+
+int divideTwoInteger(int dividend, int divisor) {
+
+  if (dividend == divisor)
+    return 1;
+
+  bool sign = true;
+  if (dividend <= 0 && divisor > 0)
+    sign = false;
+  if (dividend >= 0 && divisor < 0)
+    sign = false;
+
+  long n = abs(dividend);
+  long d = abs(divisor);
+  divisor = abs(divisor);
+
+  long quotient = 0;
+
+  while (n >= d) {
+    int count = 0;
+    while (n >= (d << (count + 1)))
+      count += 1;
+
+    quotient += 1 << count;
+    n -= (d << count);
+  }
+
+  if (quotient == (1 << 31) && sign)
+    return INT_MAX;
+  if (quotient == (1 << 31) && !sign)
+    return INT_MIN;
+
+  return sign ? quotient : -quotient;
+}
+
+int main() {
+  int n = 10, d = 3;
+  int result = divideTwoInteger(n, d);
+  cout << result << endl;
+  return 0;
+}
