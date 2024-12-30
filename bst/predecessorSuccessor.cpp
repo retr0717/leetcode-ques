f2a453aaf0dbbdd3d314d8909fd57e3eff7a10f0
retr0717 <alwinaji717@gmail.com>
2024-12-30 18:51:02 +0530

    finding the successor and predecessor of given node

---
+++ bst/predecessorSuccessor.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*************************************************************
+
+    Following is the Binary Tree node structure
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left),
+right(right) {}
+    };
+
+*************************************************************/
+
+pair<int, int> predecessorSuccessor(TreeNode *root, int key) {
+  int successor = -1;
+  int predecessor = -1;
+
+  TreeNode *curr = root;
+
+  while (curr != NULL) {
+    if (key >= curr->data)
+      curr = curr->right;
+    else {
+      successor = curr->data;
+      curr = curr->left;
+    }
+  }
+
+  curr = root;
+  while (curr != NULL) {
+    if (key <= curr->data)
+      curr = curr->left;
+    else {
+      predecessor = curr->data;
+      curr = curr->right;
+    }
+  }
+
+  return {predecessor, successor};
+}
