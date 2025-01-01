94d955c6205683234fea5e824fe8fb45e4674a1e
retr0717 <alwinaji717@gmail.com>
2025-01-01 17:16:53 +0530

    graph representations : weighted added

--- graph/graphRpr.cpp
+++ graph/graphRpr.cpp
@@ -1,6 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void matrixRprWeighted() {
+  int n, m;
+
+  cin >> n >> m;
+  int adj[n + 1][m + 1];
+
+  for (int i = 0; i < m; i++) {
+    int u, v, wt;
+    cin >> u >> v >> wt;
+
+    adj[u][v] = wt;
+    adj[v][u] = wt;
+  }
+}
+
 void matrixRpr() {
   int n, m;
 
@@ -13,6 +28,21 @@ void matrixRpr() {
 
     adj[u][v] = 1;
     adj[v][u] = 1;
+  }
+}
+
+void adjListWeighted() {
+  int n, m;
+
+  cin >> n >> m;
+  vector<vector<pair<int, int>>> adj(n + 1);
+
+  for (int i = 0; i < m; i++) {
+    int u, v, wt;
+    cin >> u >> v >> wt;
+
+    adj[u].push_back({v, wt});
+    adj[v].push_back({u, wt});
   }
 }
 
