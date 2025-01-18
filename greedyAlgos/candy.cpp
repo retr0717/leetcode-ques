0e81c218faa3333cb3b3dada9f799af5badd5822
retr0717 <alwinaji717@gmail.com>
2025-01-18 12:00:00 +0530

    canday

---
+++ greedyAlgos/candy.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  int candy(vector<int> &ratings) {
+    int sum = 1;
+    int i = 1;
+    int n = ratings.size();
+
+    while (i < n) {
+      if (ratings[i] == ratings[i - 1]) {
+        sum++;
+        i++;
+        continue;
+      }
+
+      int peak = 1;
+      while (i < n && ratings[i] > ratings[i - 1]) {
+        peak++;
+        sum += peak;
+        i++;
+      }
+
+      int down = 1;
+
+      while (i < n && ratings[i] < ratings[i - 1]) {
+        sum += down;
+        down++;
+        i++;
+      }
+
+      if (down > peak)
+        sum += (down - peak);
+    }
+
+    return sum;
+  }
+};
