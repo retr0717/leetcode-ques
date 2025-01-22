d0f265344322e5b7a00c91a4df9ff2bb2c1e900c
retr0717 <alwinaji717@gmail.com>
2025-01-22 23:12:45 +0530

    jump game 1

---
+++ greedyAlgos/jumpGame.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  bool canJump(vector<int> &nums) {
+    int maxIndex = 0;
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++) {
+      if (i > maxIndex)
+        return false;
+
+      maxIndex = max(maxIndex, i + nums[i]);
+    }
+
+    return true;
+  }
+};
