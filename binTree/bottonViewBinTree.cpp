938fcde794d7bc5de9d1aacc278936f0c83d9147
retr0717 <alwinaji717@gmail.com>
2025-02-02 01:41:25 +0530

    Bottom View

---
+++ binTree/bottonViewBinTree.cpp
@@ -0,0 +1,45 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T> class TreeNode {
+public:
+  T data;
+  TreeNode<T> *left;
+  TreeNode<T> *right;
+
+  TreeNode(T data) {
+    this->data = data;
+    left = NULL;
+    right = NULL;
+  }
+};
+
+vector<int> bottomView(TreeNode<int> *root) {
+  vector<int> ans;
+  if (root == NULL)
+    return ans;
+
+  map<int, int> mpp;
+  queue<pair<TreeNode<int> *, int>> q;
+  q.push({root, 0});
+
+  while (!q.empty()) {
+    auto it = q.front();
+    q.pop();
+
+    TreeNode<int> *node = it.first;
+    int line = it.second;
+
+    mpp[line] = node->data;
+
+    if (node->left != NULL)
+      q.push({node->left, line - 1});
+    if (node->right != NULL)
+      q.push({node->right, line + 1});
+  }
+
+  for (auto it : mpp) {
+    ans.push_back(it.second);
+  }
+}
