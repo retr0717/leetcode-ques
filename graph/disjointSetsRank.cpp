d01da2a6aace16a5b58c2e6c2872496095223f12
retr0717 <alwinaji717@gmail.com>
2025-06-03 19:40:45 +0530

    disjoint sets by rank

--- graph/disjointSetsRank.cpp
+++ graph/disjointSetsRank.cpp
@@ -24,6 +24,7 @@ public:
         int uParent = findParent(u);
         int vParent = findParent(v);
         if(uParent == vParent) return;
+        //path compression.
         if(rank[uParent] < rank[vParent]){
             parent[uParent] = vParent;
         } else if(rank[vParent] < rank[uParent]){
@@ -37,11 +38,26 @@ public:
 
 int main()
 {
-    DisjointSets ds(5);
+    DisjointSets ds(7);
     ds.unionByRank(1, 2);
     ds.unionByRank(2, 3);
     ds.unionByRank(4, 5);
-    ds.unionByRank(3, 4);
+    ds.unionByRank(6, 7);
+    ds.unionByRank(5, 6);
+
+    if(ds.findParent(3) == ds.findParent(7)) {
+        cout << "same component." << endl;
+    } else {
+        cout << "different components." << endl;
+    }
+
+    ds.unionByRank(3, 7);
+    if(ds.findParent(3) == ds.findParent(7)) {
+        cout << "same component." << endl;
+    } else {
+        cout << "different components." << endl;
+    }
+
     for(int i = 1; i <= 5; i++){
         cout << "Parent of " << i << " is: " << ds.findParent(i) << endl;
     }
