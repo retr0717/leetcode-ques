fc739abc0adad96c0b954449a9dc0795cc3251dc
ALWIN AJI <110730876+retr0717@users.noreply.github.com>
2025-07-10 14:16:00 +0530

    frog jump using memoization

---
+++ dynamic-prog/frogJumpMemo.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+
+int func(int index, vector<int> &dp, vector<int> &heights)
+{
+    if(index == 0) return 0;
+
+    if(dp[index] != -1) return dp[index];
+
+    //try all possibilities.
+    int left = func(index-1, dp, heights) + abs(heights[index] - heights[index-1]);
+    int right = INT_MAX;
+
+    if(index > 1){
+        right = func(index-2, dp, heights) + abs(heights[index] - heights[index-2]);
+    }
+
+    return dp[index] = min(left, right);
+}
+
+int frogJump(int n, vector<int> &heights)
+{
+    vector<int> dp(n+1, -1);
+    return func(n-1, dp, heights);
+}
