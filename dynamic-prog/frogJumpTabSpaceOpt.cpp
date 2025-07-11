c57b4700ca327b304c0053c7b3df25c0b2ebfeac
ALWIN AJI <110730876+retr0717@users.noreply.github.com>
2025-07-11 17:13:00 +0530

    frog jump space optimization

---
+++ dynamic-prog/frogJumpTabSpaceOpt.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+
+int frogJump(int n, vector<int> &heights)
+{
+    int prev = 0, prev2 = 0;
+
+    for(int i = 1; i < n; i++)
+    {
+        int fs = prev + abs(heights[i] - heights[i-1]);
+        int ss = INT_MAX;
+        if(i > 1) ss = prev2 + abs(heights[i] - heights[i-2]);
+
+        int curr = min(fs, ss);
+
+        prev2 = prev;
+        prev = curr;
+    }
+
+    return prev;
+}
