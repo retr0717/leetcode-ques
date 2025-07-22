988815f98d1fb9d31e097982d67f72c749a1a83d
ALWIN AJI <110730876+retr0717@users.noreply.github.com>
2025-07-22 20:00:25 +0530

    triangle min path using tabulation

---
+++ dynamic-prog/trianglePathTab.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+
+        int n = triangle.size();
+        vector<vector<int>> dp;
+        for (int i = 0; i < n; ++i)
+            dp.push_back(vector<int>(triangle[i].size(), -1));
+        
+        for(int i = 0; i < n ; i++)
+            dp[n-1][i] = triangle[n-1][i];
+
+        for(int i = n-2 ; i >= 0 ; i--)
+        {
+            for(int j = i ; j >= 0 ; j--)
+            {
+                int down = triangle[i][j] + dp[i+1][j];
+                int diag = triangle[i][j] + dp[i+1][j+1];
+
+                dp[i][j] = min(down, diag);
+            }
+        }
+
+        return dp[0][0];
+    }
+};
