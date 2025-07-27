912ecac79f2ce433a1c3dbb6191548394acfedfc
ALWIN AJI <110730876+retr0717@users.noreply.github.com>
2025-07-27 11:32:00 +0530

    count subset with sum k with min abs diff using tabulation

--- dynamic-prog/countSubWithMinSumTab.cpp
+++ dynamic-prog/countSubWithMinSumTab.cpp
@@ -1,26 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int func(int index, int target, vector<int> &arr, vector<vector<int>> &dp)
-{
-        if(index == 0)
-        {
-                if(target == 0 && arr[0] == 0)
-                        return 2;
-                if(target == 0)
-                        return 1;
-        }
-
-        if(dp[index][target] != -1) return dp[index][target];
-
-        int notTake = Count(arr, tar, index-1, dp);
-        int take = 0;
-        if( tar >= arr[index])
-                take = Count(arr, tar - arr[index], index-1, dp);
-
-        return dp[index][target] = take+notTake;
-}
-
 int mod =(int)1e9+7;
 
 int findWays(vector<int> &arr, int k)
