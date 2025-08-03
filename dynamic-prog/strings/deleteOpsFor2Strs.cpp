8c70f5864fae701ad9ea3d1ce4766e35723f5cfe
ALWIN AJI <110730876+retr0717@users.noreply.github.com>
2025-08-03 11:03:07 +0530

    Delete Operation for Two Strings

---
+++ dynamic-prog/strings/deleteOpsFor2Strs.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+
+        if(n == 1 && m == 1)
+        {
+            if(text1[0] != text2[0]) return 0;
+        }
+
+        vector<int> prev(m+1, 0), curr(m+1, 0);
+
+        for(int j = 0 ; j < m ; j++) prev[j] = 0;
+
+        for(int i = 1; i <= n ; i++)
+        {
+            for(int j = 1 ; j <= m ; j++)
+            {
+                //match found.
+                if(text1[i-1] == text2[j-1]) 
+                    curr[j] = 1 + prev[j-1];
+                else
+                    curr[j] = 0 + max(prev[j], curr[j-1]);//not a match.
+            }
+
+            prev = curr;
+        }
+
+        return prev[m];
+    }
+
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        
+
+        return (m+n) - (2* longestCommonSubsequence(word1, word2));
+    }
+};
