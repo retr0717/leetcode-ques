df94dec060dd4fb1e8ccbda20ffe220fc9c8d1ea
ALWIN AJI <110730876+retr0717@users.noreply.github.com>
2025-08-03 12:31:02 +0530

    distinct subsequences using memoization 2

---
+++ dynamic-prog/strings/distinctSubSeqMemo2.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    int distinct(int i, int j, string s, string t, vector<vector<int>> &dp)
+    {
+        if( j == 0 ) return 1;
+        if( i == 0 ) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s[i-1] == t[j-1])
+            return dp[i][j] = distinct(i-1, j-1, s, t, dp) + distinct(i-1, j , s, t, dp);
+        else
+            return dp[i][j] = distinct(i-1, j, s, t, dp);
+    }
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
+        return distinct(n, m, s, t, dp);
+    }
+};
