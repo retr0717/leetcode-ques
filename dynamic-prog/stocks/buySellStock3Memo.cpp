1896071068747a236a795fbadfbd854c458f5423
ALWIN AJI <110730876+retr0717@users.noreply.github.com>
2025-08-08 09:03:21 +0530

    buy sell stock 3 using memoization

---
+++ dynamic-prog/stocks/buySellStock3Memo.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    int f(int index, bool buy, int cap, vector<int>& prices, int n, vector<vector<int>>& dp)
+    {
+        if(cap == 0 || index == n)
+            return 0;
+
+        if(dp[index][buy] != -1) return dp[index][buy];
+
+        int profit = 0;
+        if(buy)
+        {
+            profit = max(-prices[index] + f(index+1, 0, cap, prices, n, dp), f(index+1, 1, cap, prices, n, dp)); 
+        }
+        else
+        {
+            profit = max(prices[index] + f(index+1, 1, cap-1, prices, n, dp), f(index+1, 0, cap, prices, n, dp));
+        }
+
+        return dp[index][buy] = profit;
+    }
+
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int> (2, -1));
+        return f(0, 1, 2, prices, n, dp); 
+    }
+};
