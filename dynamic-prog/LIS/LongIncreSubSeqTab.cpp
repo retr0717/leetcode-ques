e8ec3a2b1db79afd0fcea442e7b6a076cd49de71
retr0717 <alwinaji717@gmail.com>
2025-09-14 18:48:42 +0530

    long increasing sub seq using tabulation

---
+++ dynamic-prog/LIS/LongIncreSubSeqTab.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+
+        int len = 0;
+
+        for(int ind = n-1 ; ind >= 0 ; ind--)
+        {
+            for(int prevInd = ind - 1 ; prevInd >= -1 ; prevInd--)
+            {
+                len = 0 + dp[ind+1][prevInd + 1];
+                if(prevInd == -1 || nums[ind] > nums[prevInd])
+                    len = max(len, 1+dp[ind+1][ind + 1]);
+                
+                dp[ind][prevInd+1] = len;
+            }
+        }
+
+        return dp[0][0];
+    }
+};
