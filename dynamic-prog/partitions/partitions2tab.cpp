3efae65cd6002f7d3955b644f1559f8cbe1782af
ALWIN AJI <110730876+retr0717@users.noreply.github.com>
2026-02-11 07:37:08 +0530

    partitions 2 tabulation

---
+++ dynamic-prog/partitions/partitions2tab.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    bool isPalindrome(int i, int j, string s)
+    {
+        while(i < j)
+        {
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+public:
+    int minCut(string s) {
+        int  n = s.size();
+        vector<int> dp(n+1, 0);
+        
+        for(int i = n-1; i >= 0 ; i--)
+        {
+            int minCost = INT_MAX;
+            for(int j = i; j < n; j++)
+            {
+                if(isPalindrome(i, j, s))
+                {
+                    int cost = 1+dp[j+1];
+                    minCost = min(minCost, cost);
+                }
+            }
+
+            dp[i]=minCost;
+        }
+
+        return dp[0]-1;
+    }
+};
+
+int main()
+{
+    string s = "aab";
+
+    Solution obj;
+
+    cout<<obj.minCut(s)<<endl;
+
+    return 0;
+}
