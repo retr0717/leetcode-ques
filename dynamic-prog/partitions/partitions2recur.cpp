1dc1a01f76e2112d23d5794baf8ef5a3646c43d4
ALWIN AJI <110730876+retr0717@users.noreply.github.com>
2026-02-11 07:26:04 +0530

    partitions 2 recursion

---
+++ dynamic-prog/partitions/partitions2recur.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    bool isPalindrome(int i, int j, string s)
+    {
+        while(i < j)
+        {
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+    int f(int i, int n, string s)
+    {
+        if(i == n) return 0;
+
+        int minCost = INT_MAX;
+        for(int j = i; j < n; j++)
+        {
+            if(isPalindrome(i, j, s))
+            {
+                int cost = 1+f(j+1, n , s);
+                minCost = min(minCost, cost);
+            }
+        }
+
+        return minCost;
+    }
+public:
+    int minCut(string s) {
+        int  n = s.size();
+        return f(0, n, s) - 1;
+    }
+};
+
+int main()
+{
+    string s = "aab";
+
+    Solution obj;
+
+    cout<<obj.minCut(s)<<endl;
+
+    return 0;
+}
